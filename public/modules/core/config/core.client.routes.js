1298c7a7d59bb8d4eb12185ed95d6d76411609e4
Lenon Rodrigues <vrx_lfrancisco@uolinc.com>
2014-09-15 09:47:18 -0300

    alterações de css do Santana

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -12,6 +12,18 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
 		}).
+		state('roupas', {
+			url: '/roupas',
+			templateUrl: 'modules/core/views/home.client.view.html'
+		}).
+		state('eletronicos', {
+			url: '/eletronicos',
+			templateUrl: 'modules/core/views/home.client.view.html'
+		}).
+		state('top', {
+			url: '/top',
+			templateUrl: 'modules/core/views/home.client.view.html'
+		}).
         state('admin', {
 			url: '/admin',
 			templateUrl: 'modules/landingpages/views/list-landingpages.client.view.html'
