c5a7b6255d64d913115918ae6d902ed8aa00472a
Lenon Rodrigues <vrx_lfrancisco@uolinc.com>
2014-09-26 08:25:46 -0300

    asd

--- public/dist/app.js
+++ public/dist/app.js
@@ -43,6 +43,23 @@ angular.element(document).ready(function() {
 });
 'use strict';
 
+// Use Applicaion configuration module to register a new module
+ApplicationConfiguration.registerModule('core');
+'use strict';
+
+// Use applicaion configuration module to register a new module
+ApplicationConfiguration.registerModule('emails');
+'use strict';
+
+// Use applicaion configuration module to register a new module
+ApplicationConfiguration.registerModule('landingpages');
+'use strict';
+
+// Use Applicaion configuration module to register a new module
+ApplicationConfiguration.registerModule('users');
+
+'use strict';
+
 // Setting up route
 angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
@@ -265,10 +282,6 @@ angular.module('core').controller('HomeController', ['$scope', '$location', 'Aut
 ]);
 'use strict';
 
-// Use Applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('core');
-'use strict';
-
 //Menu service used for managing  menus
 angular.module('core').service('Menus', [
 
@@ -433,54 +446,6 @@ angular.module('core').service('Menus', [
 		this.addMenu('topbar');
 	}
 ]);
-'use strict';
-
-(function() {
-	describe('HeaderController', function() {
-		//Initialize global variables
-		var scope,
-			HeaderController;
-
-		// Load the main application module
-		beforeEach(module(ApplicationConfiguration.applicationModuleName));
-
-		beforeEach(inject(function($controller, $rootScope) {
-			scope = $rootScope.$new();
-
-			HeaderController = $controller('HeaderController', {
-				$scope: scope
-			});
-		}));
-
-		it('should expose the authentication service', function() {
-			expect(scope.authentication).toBeTruthy();
-		});
-	});
-})();
-'use strict';
-
-(function() {
-	describe('HomeController', function() {
-		//Initialize global variables
-		var scope,
-			HomeController;
-
-		// Load the main application module
-		beforeEach(module(ApplicationConfiguration.applicationModuleName));
-
-		beforeEach(inject(function($controller, $rootScope) {
-			scope = $rootScope.$new();
-
-			HomeController = $controller('HomeController', {
-				$scope: scope
-			});
-		}));
-
-		it('should expose the authentication service', function() {
-			expect(scope.authentication).toBeTruthy();
-		});
-	});
-})();
 'use strict';
 
 // Configuring the Articles module
@@ -672,10 +637,6 @@ angular.module('emails').controller('EmailsController', ['$scope', '$stateParams
 ]);
 'use strict';
 
-// Use applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('emails');
-'use strict';
-
 //Emails service used to communicate Emails REST endpoints
 angular.module('emails').factory('Emails', ['$resource',
 	function($resource) {
@@ -689,169 +650,6 @@ angular.module('emails').factory('Emails', ['$resource',
 		});
 	}
 ]);
-'use strict';
-
-(function() {
-	// Emails Controller Spec
-	describe('Emails Controller Tests', function() {
-		// Initialize global variables
-		var EmailsController,
-		scope,
-		$httpBackend,
-		$stateParams,
-		$location;
-
-		// The $resource service augments the response object with methods for updating and deleting the resource.
-		// If we were to use the standard toEqual matcher, our tests would fail because the test values would not match
-		// the responses exactly. To solve the problem, we define a new toEqualData Jasmine matcher.
-		// When the toEqualData matcher compares two objects, it takes only object properties into
-		// account and ignores methods.
-		beforeEach(function() {
-			jasmine.addMatchers({
-				toEqualData: function(util, customEqualityTesters) {
-					return {
-						compare: function(actual, expected) {
-							return {
-								pass: angular.equals(actual, expected)
-							};
-						}
-					};
-				}
-			});
-		});
-
-		// Then we can start by loading the main application module
-		beforeEach(module(ApplicationConfiguration.applicationModuleName));
-
-		// The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
-		// This allows us to inject a service but then attach it to a variable
-		// with the same name as the service.
-		beforeEach(inject(function($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_) {
-			// Set a new global scope
-			scope = $rootScope.$new();
-
-			// Point global variables to injected services
-			$stateParams = _$stateParams_;
-			$httpBackend = _$httpBackend_;
-			$location = _$location_;
-
-			// Initialize the Emails controller.
-			EmailsController = $controller('EmailsController', {
-				$scope: scope
-			});
-		}));
-
-		it('$scope.find() should create an array with at least one Email object fetched from XHR', inject(function(Emails) {
-			// Create sample Email using the Emails service
-			var sampleEmail = new Emails({
-				name: 'New Email'
-			});
-
-			// Create a sample Emails array that includes the new Email
-			var sampleEmails = [sampleEmail];
-
-			// Set GET response
-			$httpBackend.expectGET('emails').respond(sampleEmails);
-
-			// Run controller functionality
-			scope.find();
-			$httpBackend.flush();
-
-			// Test scope value
-			expect(scope.emails).toEqualData(sampleEmails);
-		}));
-
-		it('$scope.findOne() should create an array with one Email object fetched from XHR using a emailId URL parameter', inject(function(Emails) {
-			// Define a sample Email object
-			var sampleEmail = new Emails({
-				name: 'New Email'
-			});
-
-			// Set the URL parameter
-			$stateParams.emailId = '525a8422f6d0f87f0e407a33';
-
-			// Set GET response
-			$httpBackend.expectGET(/emails\/([0-9a-fA-F]{24})$/).respond(sampleEmail);
-
-			// Run controller functionality
-			scope.findOne();
-			$httpBackend.flush();
-
-			// Test scope value
-			expect(scope.email).toEqualData(sampleEmail);
-		}));
-
-		it('$scope.create() with valid form data should send a POST request with the form input values and then locate to new object URL', inject(function(Emails) {
-			// Create a sample Email object
-			var sampleEmailPostData = new Emails({
-				name: 'New Email'
-			});
-
-			// Create a sample Email response
-			var sampleEmailResponse = new Emails({
-				_id: '525cf20451979dea2c000001',
-				name: 'New Email'
-			});
-
-			// Fixture mock form input values
-			scope.name = 'New Email';
-
-			// Set POST response
-			$httpBackend.expectPOST('emails', sampleEmailPostData).respond(sampleEmailResponse);
-
-			// Run controller functionality
-			scope.create();
-			$httpBackend.flush();
-
-			// Test form inputs are reset
-			expect(scope.name).toEqual('');
-
-			// Test URL redirection after the Email was created
-			expect($location.path()).toBe('/emails/' + sampleEmailResponse._id);
-		}));
-
-		it('$scope.update() should update a valid Email', inject(function(Emails) {
-			// Define a sample Email put data
-			var sampleEmailPutData = new Emails({
-				_id: '525cf20451979dea2c000001',
-				name: 'New Email'
-			});
-
-			// Mock Email in scope
-			scope.email = sampleEmailPutData;
-
-			// Set PUT response
-			$httpBackend.expectPUT(/emails\/([0-9a-fA-F]{24})$/).respond();
-
-			// Run controller functionality
-			scope.update();
-			$httpBackend.flush();
-
-			// Test URL location to new object
-			expect($location.path()).toBe('/emails/' + sampleEmailPutData._id);
-		}));
-
-		it('$scope.remove() should send a DELETE request with a valid emailId and remove the Email from the scope', inject(function(Emails) {
-			// Create new Email object
-			var sampleEmail = new Emails({
-				_id: '525a8422f6d0f87f0e407a33'
-			});
-
-			// Create new Emails array and include the Email
-			scope.emails = [sampleEmail];
-
-			// Set expected DELETE response
-			$httpBackend.expectDELETE(/emails\/([0-9a-fA-F]{24})$/).respond(204);
-
-			// Run controller functionality
-			scope.remove(sampleEmail);
-			$httpBackend.flush();
-
-			// Test array after successful delete
-			expect(scope.emails.length).toBe(0);
-		}));
-	});
-}());
 'use strict';
 
 // Configuring the Articles module
@@ -1269,10 +1067,6 @@ angular.module('landingpages').controller('LandingpagesController', ['$scope', '
 ]);
 'use strict';
 
-// Use applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('landingpages');
-'use strict';
-
 //Landingpages service used to communicate Landingpages REST endpoints
 angular.module('landingpages').factory('Landingpages', ['$resource',
 	function($resource) {
@@ -1288,169 +1082,6 @@ angular.module('landingpages').factory('Landingpages', ['$resource',
 		});
 	}
 ]);
-'use strict';
-
-(function() {
-	// Landingpages Controller Spec
-	describe('Landingpages Controller Tests', function() {
-		// Initialize global variables
-		var LandingpagesController,
-		scope,
-		$httpBackend,
-		$stateParams,
-		$location;
-
-		// The $resource service augments the response object with methods for updating and deleting the resource.
-		// If we were to use the standard toEqual matcher, our tests would fail because the test values would not match
-		// the responses exactly. To solve the problem, we define a new toEqualData Jasmine matcher.
-		// When the toEqualData matcher compares two objects, it takes only object properties into
-		// account and ignores methods.
-		beforeEach(function() {
-			jasmine.addMatchers({
-				toEqualData: function(util, customEqualityTesters) {
-					return {
-						compare: function(actual, expected) {
-							return {
-								pass: angular.equals(actual, expected)
-							};
-						}
-					};
-				}
-			});
-		});
-
-		// Then we can start by loading the main application module
-		beforeEach(module(ApplicationConfiguration.applicationModuleName));
-
-		// The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
-		// This allows us to inject a service but then attach it to a variable
-		// with the same name as the service.
-		beforeEach(inject(function($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_) {
-			// Set a new global scope
-			scope = $rootScope.$new();
-
-			// Point global variables to injected services
-			$stateParams = _$stateParams_;
-			$httpBackend = _$httpBackend_;
-			$location = _$location_;
-
-			// Initialize the Landingpages controller.
-			LandingpagesController = $controller('LandingpagesController', {
-				$scope: scope
-			});
-		}));
-
-		it('$scope.find() should create an array with at least one Landingpage object fetched from XHR', inject(function(Landingpages) {
-			// Create sample Landingpage using the Landingpages service
-			var sampleLandingpage = new Landingpages({
-				name: 'New Landingpage'
-			});
-
-			// Create a sample Landingpages array that includes the new Landingpage
-			var sampleLandingpages = [sampleLandingpage];
-
-			// Set GET response
-			$httpBackend.expectGET('landingpages').respond(sampleLandingpages);
-
-			// Run controller functionality
-			scope.find();
-			$httpBackend.flush();
-
-			// Test scope value
-			expect(scope.landingpages).toEqualData(sampleLandingpages);
-		}));
-
-		it('$scope.findOne() should create an array with one Landingpage object fetched from XHR using a landingpageId URL parameter', inject(function(Landingpages) {
-			// Define a sample Landingpage object
-			var sampleLandingpage = new Landingpages({
-				name: 'New Landingpage'
-			});
-
-			// Set the URL parameter
-			$stateParams.landingpageId = '525a8422f6d0f87f0e407a33';
-
-			// Set GET response
-			$httpBackend.expectGET(/landingpages\/([0-9a-fA-F]{24})$/).respond(sampleLandingpage);
-
-			// Run controller functionality
-			scope.findOne();
-			$httpBackend.flush();
-
-			// Test scope value
-			expect(scope.landingpage).toEqualData(sampleLandingpage);
-		}));
-
-		it('$scope.create() with valid form data should send a POST request with the form input values and then locate to new object URL', inject(function(Landingpages) {
-			// Create a sample Landingpage object
-			var sampleLandingpagePostData = new Landingpages({
-				name: 'New Landingpage'
-			});
-
-			// Create a sample Landingpage response
-			var sampleLandingpageResponse = new Landingpages({
-				_id: '525cf20451979dea2c000001',
-				name: 'New Landingpage'
-			});
-
-			// Fixture mock form input values
-			scope.name = 'New Landingpage';
-
-			// Set POST response
-			$httpBackend.expectPOST('landingpages', sampleLandingpagePostData).respond(sampleLandingpageResponse);
-
-			// Run controller functionality
-			scope.create();
-			$httpBackend.flush();
-
-			// Test form inputs are reset
-			expect(scope.name).toEqual('');
-
-			// Test URL redirection after the Landingpage was created
-			expect($location.path()).toBe('/landingpages/' + sampleLandingpageResponse._id);
-		}));
-
-		it('$scope.update() should update a valid Landingpage', inject(function(Landingpages) {
-			// Define a sample Landingpage put data
-			var sampleLandingpagePutData = new Landingpages({
-				_id: '525cf20451979dea2c000001',
-				name: 'New Landingpage'
-			});
-
-			// Mock Landingpage in scope
-			scope.landingpage = sampleLandingpagePutData;
-
-			// Set PUT response
-			$httpBackend.expectPUT(/landingpages\/([0-9a-fA-F]{24})$/).respond();
-
-			// Run controller functionality
-			scope.update();
-			$httpBackend.flush();
-
-			// Test URL location to new object
-			expect($location.path()).toBe('/landingpages/' + sampleLandingpagePutData._id);
-		}));
-
-		it('$scope.remove() should send a DELETE request with a valid landingpageId and remove the Landingpage from the scope', inject(function(Landingpages) {
-			// Create new Landingpage object
-			var sampleLandingpage = new Landingpages({
-				_id: '525a8422f6d0f87f0e407a33'
-			});
-
-			// Create new Landingpages array and include the Landingpage
-			scope.landingpages = [sampleLandingpage];
-
-			// Set expected DELETE response
-			$httpBackend.expectDELETE(/landingpages\/([0-9a-fA-F]{24})$/).respond(204);
-
-			// Run controller functionality
-			scope.remove(sampleLandingpage);
-			$httpBackend.flush();
-
-			// Test array after successful delete
-			expect(scope.landingpages.length).toBe(0);
-		}));
-	});
-}());
 'use strict';
 
 // Config HTTP Error Handling
@@ -1703,125 +1334,3 @@ angular.module('users').factory('Users', ['$resource',
 		});
 	}
 ]);
-'use strict';
-
-(function() {
-	// Authentication controller Spec
-	describe('AuthenticationController', function() {
-		// Initialize global variables
-		var AuthenticationController,
-			scope,
-			$httpBackend,
-			$stateParams,
-			$location;
-
-		beforeEach(function() {
-			jasmine.addMatchers({
-				toEqualData: function(util, customEqualityTesters) {
-					return {
-						compare: function(actual, expected) {
-							return {
-								pass: angular.equals(actual, expected)
-							};
-						}
-					};
-				}
-			});
-		});
-
-		// Load the main application module
-		beforeEach(module(ApplicationConfiguration.applicationModuleName));
-
-		// The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
-		// This allows us to inject a service but then attach it to a variable
-		// with the same name as the service.
-		beforeEach(inject(function($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_) {
-			// Set a new global scope
-			scope = $rootScope.$new();
-
-			// Point global variables to injected services
-			$stateParams = _$stateParams_;
-			$httpBackend = _$httpBackend_;
-			$location = _$location_;
-
-			// Initialize the Authentication controller
-			AuthenticationController = $controller('AuthenticationController', {
-				$scope: scope
-			});
-		}));
-
-
-		it('$scope.signin() should login with a correct user and password', function() {
-			// Test expected GET request
-			$httpBackend.when('POST', '/auth/signin').respond(200, 'Fred');
-
-			scope.signin();
-			$httpBackend.flush();
-
-			// Test scope value
-			expect(scope.authentication.user).toEqual('Fred');
-			expect($location.url()).toEqual('/');
-		});
-
-		it('$scope.signin() should fail to log in with nothing', function() {
-			// Test expected POST request
-			$httpBackend.expectPOST('/auth/signin').respond(400, {
-				'message': 'Missing credentials'
-			});
-
-			scope.signin();
-			$httpBackend.flush();
-
-			// Test scope value
-			expect(scope.error).toEqual('Missing credentials');
-		});
-
-		it('$scope.signin() should fail to log in with wrong credentials', function() {
-			// Foo/Bar combo assumed to not exist
-			scope.authentication.user = 'Foo';
-			scope.credentials = 'Bar';
-
-			// Test expected POST request
-			$httpBackend.expectPOST('/auth/signin').respond(400, {
-				'message': 'Unknown user'
-			});
-
-			scope.signin();
-			$httpBackend.flush();
-
-			// Test scope value
-			expect(scope.error).toEqual('Unknown user');
-		});
-
-		it('$scope.signup() should register with correct data', function() {
-			// Test expected GET request
-			scope.authentication.user = 'Fred';
-			$httpBackend.when('POST', '/auth/signup').respond(200, 'Fred');
-
-			scope.signup();
-			$httpBackend.flush();
-
-			// test scope value
-			expect(scope.authentication.user).toBe('Fred');
-			expect(scope.error).toEqual(undefined);
-			expect($location.url()).toBe('/');
-		});
-
-		it('$scope.signup() should fail to register with duplicate Username', function() {
-			// Test expected POST request
-			$httpBackend.when('POST', '/auth/signup').respond(400, {
-				'message': 'Username already exists'
-			});
-
-			scope.signup();
-			$httpBackend.flush();
-
-			// Test scope value
-			expect(scope.error).toBe('Username already exists');
-		});
-	});
-}());
-'use strict';
-
-// Use Applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('users');
