ba59785e4230f62ac6dcb99bad41831cf76c6ca8
Lenon Rodrigues <lenonrf@MacBook-Pro-de-Lenon.local>
2014-09-28 10:06:24 -0300

    teste de minificação

--- config/init.js
+++ config/init.js
@@ -21,12 +21,13 @@ module.exports = function() {
 
 		if (!environmentFiles.length) {
 
+/*
 			if (process.env.NODE_ENV) {
 				console.error('\x1b[31m', 'No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead');
 			} else {
 				console.error('\x1b[31m', 'NODE_ENV is not defined! Using default development environment');
 			}
-
+*/
 			process.env.NODE_ENV = 'development';
 			
 		} else {
